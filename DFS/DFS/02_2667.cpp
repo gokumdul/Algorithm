25e79d00f3062bc4ed2db9968847926102ee9681
Gokum <ssjk0802@naver.com>
2021-04-03 23:53:38 +0900

    2667 단지번호붙이기 dfs

--- DFS/DFS/02_2667.cpp
+++ DFS/DFS/02_2667.cpp
@@ -4,65 +4,64 @@
 
 // DFS
 
-#include<iostream>
-#include<vector>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define MAX 25
-
 int N;
-int arr[MAX][MAX];
-bool visit[MAX][MAX];
-int house_cnt;
+int arr[25][25];
+bool isVisited[25][25];
+int cnt = 0;
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
+vector<int> b;
 
-vector <int> vec;
-
-void DFS(int x, int y) {
-	house_cnt++;
-	visit[x][y] = true;
+void dfs(int x, int y) {
+	cnt++;
+	isVisited[x][y] = 1;
 
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
 		int ny = y + dy[i];
-		if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-		if (arr[nx][ny] == 1 && visit[nx][ny] == false) {
-			DFS(nx, ny);
-		}
+		if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+			continue;
+		if (arr[nx][ny] == 1 && !isVisited[nx][ny])
+			dfs(nx, ny);
 	}
+
 }
+
 
 int main() {
 	cin >> N;
 
+	//input에 공백없이 들어오므로,
 	for (int i = 0; i < N; i++) {
-		string temp;
-		cin >> temp;
-		for (int j = 0; j < N; j++)	arr[i][j] = temp[j] - '0';
+		string tmp;
+		cin >> tmp;
+		for (int j = 0; j < N; j++)
+			arr[i][j] = tmp[j] - '0';
 	}
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (arr[i][j] == 1 && visit[i][j] == false) {
-				house_cnt = 0;
-				DFS(i, j);
-				vec.push_back(house_cnt);
+
+	for (int x = 0; x < N; x++) {
+		for (int y = 0; y < N; y++) {
+			if (arr[x][y] == 1 && !isVisited[x][y]) {
+				cnt = 0;
+				dfs(x, y);
+				b.push_back(cnt);
 			}
 		}
 	}
 
-	sort(vec.begin(), vec.end());
-	cout << vec.size() << endl;
-
-	for (int i = 0; i < vec.size(); i++) cout << vec[i] << endl;
+	sort(b.begin(), b.end());
+	cout << b.size() << '\n';
+	
+	for (int i = 0; i < b.size(); i++)
+		cout << b[i] << '\n';
 
 	return 0;
 }
-
