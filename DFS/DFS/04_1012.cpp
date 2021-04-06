1df6ce1adb11bc88ae764e3c5c296b92542f5a35
Gokum <ssjk0802@naver.com>
2021-04-07 00:06:54 +0900

    1012 유기농배추 memset 헤더 추가

--- DFS/DFS/04_1012.cpp
+++ DFS/DFS/04_1012.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
@@ -14,23 +15,22 @@ int T, M, N, K;
 bool arr[50][50] = { 0 };
 bool isVisited[50][50] = { 0 };
 vector<int> ans;
-
 int cnt = 0;
+
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 
 void dfs(int x, int y) {
-	cnt++;
-	isVisited[x][y] = 1;
-
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
 		int ny = y + dy[i];
 		if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
 			continue;
 		}
-		if (arr[nx][ny] == 1 && !isVisited[nx][ny])
+		if (arr[nx][ny] == 1 && !isVisited[nx][ny]) {
+			isVisited[nx][ny] = 1;
 			dfs(nx, ny);
+		}
 	}
 
 }
@@ -41,7 +41,11 @@ int main() {
 	int x, y;
 	for (int i = 0; i < T; i++) {
 		cin >> M >> N >> K;
-		vector<int> b;
+
+		memset(arr, 0, sizeof(arr));
+		memset(isVisited, 0, sizeof(isVisited));
+		cnt = 0;
+
 		for (int j = 0; j < K; j++) {
 			cin >> x >> y;
 			arr[x][y] = 1;
@@ -49,19 +53,17 @@ int main() {
 		for (int x = 0; x < M; x++) {
 			for (int y = 0; y < N; y++) {
 				if (arr[x][y] == 1 && !isVisited[x][y]) {
-					cnt = 0;
+					cnt++;
+					isVisited[x][y] = 1;
 					dfs(x, y);
-					b.push_back(cnt);
 				}
 			}
 		}
-		ans.push_back(b.size());
+		ans.push_back(cnt);
 	}
 
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << '\n';
-	}
-
+	for (int i = 0; i < T; i++)
+		cout << ans[i] << endl;
 
 	return 0;
 }
