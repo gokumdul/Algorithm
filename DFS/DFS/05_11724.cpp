b7e3f0f20d3edee61e4d3b89fefb6be191eac12a
Gokum <ssjk0802@naver.com>
2021-04-08 23:59:12 +0900

    11724 연결요소의 개수 dfs

--- DFS/DFS/05_11724.cpp
+++ DFS/DFS/05_11724.cpp
@@ -10,9 +10,40 @@
 
 using namespace std;
 
+// N: node, M: edge
+int N, M;
+bool isVisited[1001] = { 0 };
+vector<int> arr[1001];
+
+
+void dfs(int start) {
+	isVisited[start] = 1;
+	for (int i = 0; i < arr[start].size(); i++) {
+		int next = arr[start][i];
+		if (!isVisited[next])
+			dfs(next);
+	}
+}
+
 int main() {
+	cin >> N >> M;
 
+	int x, y;
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y;
+		arr[x].push_back(y);
+		arr[y].push_back(x);
+	}
 
+	int cnt = 0;
+	for (int i = 0; i <= N; i++) {
+		if (!isVisited[i]) {
+			dfs(i);
+			cnt++;
+		}
+	}
+
+	cout << cnt << endl;
 
 	return 0;
 }
