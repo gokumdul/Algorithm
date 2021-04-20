ab89cd0579864f3d8548137e69654574d449eb18
Gokum <ssjk0802@naver.com>
2021-04-21 02:10:33 +0900

    lvl2 조이스틱 greedy 풀이2

--- Greedy Algorithm/Greedy Algorithm/12_p2.cpp
+++ Greedy Algorithm/Greedy Algorithm/12_p2.cpp
@@ -6,9 +6,12 @@
  * 테스트 케이스 11 "ABABAAAAABA"인 경우처럼
  * 오른쪽으로 갔다가 다시 왼쪽으로 되돌아 오는 경우가 최소인 경우의 방법을 고려하는 것이 핵심이었다
  * 
- ++ 음.. 더 생각해보니
- 더 최적의 해를 위해서 i 가 name.size()의 half-1 까지만 고려해도 되겠다.
- 그 이상부터는 무조건 오른쪽으로 한번에 가는게 최소 값이니깐
+ * + 음.. 더 생각해보니
+ * 더 최적의 해를 위해서 i 가 name.size()의 half 까지만 고려해도 되겠다.
+ * 그 이상부터는 무조건 오른쪽으로 한번에 가는게 최소 값이니깐
+ *
+ * ++ 최적해를 고려하여 코드 첫 for 문의 조건을 name.size()/2로 변경하였다.
+ *
  */
 
 // Greedy
@@ -22,7 +25,7 @@ int solution(string name) {
 	int answer = 0;
 	int last = 0;
 	int num = name.size()-1;
-	for (int i = 0; i < name.size(); i++) {
+	for (int i = 0; i < name.size() / 2; i++) {
 		for (int j = i+1; j < name.size(); j++) {
 			if (name[j] != 'A') {
 				last = name.size()-j;
