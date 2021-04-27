a167f8a3fd13d4c1c3178fbd1e6ddd88de404fdc
Gokum <ssjk0802@naver.com>
2021-04-27 23:50:26 +0900

    p1 완주하지 못한 선수 풀이 1(벡터)

---
+++ 해시/해시/p1_완주하지 못한 선수.cpp
@@ -0,0 +1,35 @@
+// 프로그래머스
+// lvl1
+// 완주하지 못한 선수
+
+// 해시
+
+/*
+  이번에는 vector 이용해 풀었으나
+  다음번에는 unordered map 라이브러리를 이용해서 해쉬를 이용해 풀어야겠다.
+ */
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+	for (int i = 0; i < completion.size(); i++) {
+		if (participant[i] != completion[i]) {
+			return participant[i];
+		}
+	}
+	return participant[participant.size() - 1];
+}
+
+int main() {
+	vector<string> a = { "marina", "josipa", "nikola", "vinko", "filipa" };
+	vector<string> b = { "josipa", "filipa", "marina", "nikola" };
+	cout << solution(a, b) << endl;
+	return 0;
+}
